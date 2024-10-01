527c2e78423093813bd6c8873cc2a48bc779bfe3
Faith Kariuki <tcwamboi@gmail.com>
2024-10-01 10:53:34 +0300

    Complete lab

--- server/app.py
+++ server/app.py
@@ -20,7 +20,19 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake is None:
+        return make_response({'message': f'Earthquake {id} not found.'}, 404)
+    return make_response(earthquake.to_dict(), 200)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    if not earthquakes:
+        return make_response({'count': 0, 'quakes': [], 'message': f'Earthquake with magnitude {magnitude} not found.'}, 200)
+    return make_response({'count': len(earthquakes), 'quakes': [earthquake.to_dict() for earthquake in earthquakes]}, 200)
 
 
 if __name__ == '__main__':
